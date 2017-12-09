4c9eb51f121acca9f987274d43af8433a1f89a84
Philipp Eisen <philipp.eisen@king.com>
2017-12-09 22:21:34 +0100

    first interation of input pipeline done.

--- balling/input.py
+++ balling/input.py
@@ -1,8 +1,176 @@
+from __future__ import print_function
+import os
+import os.path
+import subprocess
+import logging
+
 import tensorflow as tf
+from tensorflow.contrib.framework.python.ops import audio_ops as contrib_audio
+from tensorflow.python.ops import io_ops
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def run_with_logging(cmd):
+    """
+    Run cmd and wait for it to finish. While cmd is running, we read it's
+    output and print it to a logger.
+
+    :param cmd: command to run. Should be a list of command tokens, e.g. ['ls', '-l']
+    """
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    while True:
+        line = process.stdout.readline()
+        if not line:
+            break
+        logger.info(line.rstrip('\n'))
+
+    exit_code = process.wait()
+
+    if exit_code:
+        raise subprocess.CalledProcessError(exit_code, cmd)
+
+    return exit_code
 
 
-def input_wav_files(input_dir):
+def split_audio_files(path, outpath, extension=None, segment_duration=12):
+    """
+    Splits audio files into time segments of seconds. The output files are named after the input file, but save
+    in a specified directory
+
+    :param path: path to input files. Can be a directory or a single file
+    :param outpath: directory where output file will be written to
+    :param segment_duration: duration of each segment in seconds
+    :return:
+    """
+
+    if extension and not extension.endswith('.'):
+        extension = ''.join(['.', extension])
+
+    if os.path.isfile(path):
+        files = [path]
+    else:
+        files = get_files(path, extension)
+
+    part_suffix = '%06d'
+
+    for filepath in files:
+        if extension:
+            outfile = os.path.join(
+                outpath,
+                ''.join([os.path.basename(filepath[0:-len(extension)]), part_suffix, extension])
+            )
+
+        else:
+            outfile = os.path.join(
+                outpath,
+                ''.join([os.path.basename(filepath), part_suffix])
+            )
+
+        if not os.path.exists(os.path.dirname(outfile)):
+            os.makedirs(os.path.dirname(outfile))
+
+        cmd = [
+            'ffmpeg',
+            '-i',
+            filepath,
+            '-f',
+            'segment',
+            '-segment_time',
+            str(segment_duration),
+            '-c',
+            'copy',
+            outfile
+        ]
+
+        run_with_logging(cmd)
+
+
+def get_files(directory, extension=None):
+    r"""
+    Gets all files in a directory with an optional extension filter
+    :param directory: input directory to get the files from
+    :param extension: `optional` filter by extension
+    """
+    all_files = []
+
+    if extension and not extension.startswith('.'):
+        extension = ''.join(['.', extension])
+
+    for dirpath, subdirs, files in os.walk(directory):
+        for file_ in files:
+            if extension and file_.endswith(extension):
+                all_files.append(os.path.join(dirpath, file_))
+            elif extension is None:
+                all_files.append(os.path.join(dirpath, file_))
+    return all_files
+
+
+def parse_wav(filename):
+    r"""
+    parses a single wav file.
+    :param filename: the filename of the file to be parsed
+    """
+    with tf.Session(graph=tf.Graph()) as sess:
+        wav_filename_placeholder = tf.placeholder(tf.string, [])
+        wav_loader = io_ops.read_file(wav_filename_placeholder)
+        wav_decoder = contrib_audio.decode_wav(wav_loader,
+                                               desired_channels=1,
+                                               desired_samples=100)
+        return sess.run(
+            wav_decoder,
+        feed_dict={wav_filename_placeholder: filename}
+        ).audio.flatten()
+
+
+def audio_tensors_generator(directory,
+                            positive_label_dir_name='/play/'):
+    r"""
+    creates a generator from a directory with PCM coded tensors of all wav files
+    in the directory
+
+    :param directory the directory from where to load the files
+    """
+    filenames = get_files(directory, extension='wav')
+
+
+    for filename in filenames:
+        label = int(positive_label_dir_name in filename)
+        yield (parse_wav(filename), label)
+
+
+def get_input_data(input_dir, batch_size, repeat, buffer_size=1000):
     r"""
     :param input_dir: string of where the input data is stored.
     """
-    dataset = tf.data.Dataset()
+    dataset = tf.data.Dataset.from_generator(lambda: audio_tensors_generator(input_dir),
+    output_types=(tf.float32, tf.int64),
+    output_shapes=(tf.TensorShape([None]), tf.TensorShape([]))
+    )
+    dataset = dataset.shuffle(buffer_size=buffer_size)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.repeat(repeat)
+
+    iterator = dataset.make_one_shot_iterator()
+    return iterator.get_next()
+
+
+# if __name__ == '__main__':
+#
+#     split_audio_files(path='/Users/guilherme/Downloads/audio',
+#                       outpath='/Users/guilherme/Downloads/audio/part',
+#                       extension='wav',
+#                       segment_duration=12)
+
+
+if __name__ == '__main__':
+
+    with tf.Session() as sess:
+        feature, label = get_input_data('/Users/philippeisen/Downloads/raw',
+                                    3,
+                                    200)
+        for i in range(10):
+
+            print(sess.run([feature, label]))
