bc1440467619784c980a0b238323bcff59870e2b
Philipp Eisen <philipp.eisen@king.com>
2017-12-13 10:00:19 +0100

    WIP

--- balling/input.py
+++ balling/input.py
@@ -41,8 +41,42 @@ def filenames_and_labels_in_dir(directory, extension='wav', positive_label_path_
     return all_files, all_labels
 
 
-def mfcc_fingerprint_from_wav(filename,
-                              label,
+def parse_wav(filename):
+    wav = io_ops.read_file(filename)
+    decoded_wav = contrib_audio.decode_wav(wav,
+                                           desired_channels=1)
+    return decoded_wav
+
+
+def create_sliding_windows(decoded_wav,
+                           label,
+                           audio_snippet_len_secs,
+                           audio_snippet_stride_secs,
+                           sampling_rate_hz):
+    window_size_samples = audio_snippet_len_secs * sampling_rate_hz
+    window_stride_samples = audio_snippet_stride_secs * sampling_rate_hz
+
+    windows = tf.squeeze(
+        tf.extract_image_patches(decoded_wav.audio[None, ..., None],
+                                 ksizes=[1, window_size_samples, 1, 1],
+                                 strides=[1, window_stride_samples, 1, 1],
+                                 rates=[1, 1, 1, 1],
+                                 padding='VALID'))
+
+    return tf.data.Dataset.from_tensors(windows)
+
+    windows_datasets = [tf.data.Dataset.from_tensors((window, label)) for window in windows]
+
+    # Concatenate the datasets in the list
+    # TODO make this fail safe or change it entirely
+    data_set = windows_datasets[0]
+    for window in windows_datasets[1:]:
+        data_set.concatenate(window)
+
+    return data_set
+
+
+def mfcc_fingerprint_from_wav(decoded_wav,
                               window_size_secs,
                               stride_secs,
                               sampling_rate_hz,
@@ -54,7 +88,7 @@ def mfcc_fingerprint_from_wav(filename,
     More info on mfcc: https://en.wikipedia.org/wiki/Mel-frequency_cepstrum
 
 
-    :param filename: filename of the wav file
+    :param decoded_wav: the decoded wav file
     :param window_size_secs: the window for the audio spectrum in seconds.
     :param stride_secs: the stride for the audio spectrum in seconds.
     :param sampling_rate_hz: the rate at which the input was sampled in Hz.
@@ -66,18 +100,13 @@ def mfcc_fingerprint_from_wav(filename,
                 with shape [time_steps, dct_coefficients, 1]
     """
 
-    desired_samples = audio_length_secs * sampling_rate_hz
     window_size_samples = sampling_rate_hz * window_size_secs
     stride_samples = sampling_rate_hz * stride_secs
 
-    wav_loader = io_ops.read_file(filename)
-
-    wav_decoder = contrib_audio.decode_wav(wav_loader,
-                                           desired_channels=1,
-                                           desired_samples=desired_samples)
+    decoded_wav = tf.reshape(decoded_wav, [-1, 1])
 
     spectogram = contrib_audio.audio_spectrogram(
-        wav_decoder.audio,
+        decoded_wav,
         window_size=window_size_samples,
         stride=stride_samples,
         magnitude_squared=True
@@ -91,7 +120,7 @@ def mfcc_fingerprint_from_wav(filename,
 
     mfcc = tf.reshape(mfcc, (spectogram.shape[1], dct_coefficient_count, 1))
 
-    return mfcc, label
+    return mfcc
 
 
 def get_input_data(input_dir,
@@ -131,16 +160,28 @@ def get_input_data(input_dir,
     labels = tf.constant(labels, dtype=tf.int32)
 
     dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
-    dataset = dataset.map(
-        lambda filename, label_: mfcc_fingerprint_from_wav(filename,
-                                                           label_,
-                                                           audio_length_secs=audio_length_secs,
-                                                           sampling_rate_hz=sampling_rate_hz,
-                                                           window_size_secs=spectogram_window_size_secs,
-                                                           stride_secs=spectogram_stride_secs,
-                                                           dct_coefficient_count=dct_coefficient_count),
-        num_parallel_calls=multiprocessing.cpu_count()
+    dataset = dataset.map(lambda filename, label_: (parse_wav(filename),
+                                                    label_),
+                          num_parallel_calls=multiprocessing.cpu_count())
+
+    dataset = dataset.flat_map(
+        lambda decoded_wav, label_: (create_sliding_windows(decoded_wav,
+                                                            label_,
+                                                            audio_snippet_len_secs=12,
+                                                            audio_snippet_stride_secs=2,
+                                                            sampling_rate_hz=sampling_rate_hz))
     )
+
+    dataset = dataset.map(
+        lambda decoded_wav, label_: (mfcc_fingerprint_from_wav(decoded_wav,
+                                                               audio_length_secs=audio_length_secs,
+                                                               sampling_rate_hz=sampling_rate_hz,
+                                                               window_size_secs=spectogram_window_size_secs,
+                                                               stride_secs=spectogram_stride_secs,
+                                                               dct_coefficient_count=dct_coefficient_count),
+                                     label_),
+        num_parallel_calls=multiprocessing.cpu_count())
+
     dataset = dataset.shuffle(buffer_size=shuffle_buffer_size)
     dataset = dataset.batch(batch_size)
     dataset = dataset.repeat(epochs)
@@ -152,8 +193,10 @@ def get_input_data(input_dir,
 if __name__ == '__main__':
 
     with tf.Session() as sess:
-        feature, label = get_input_data('data/splits',
+        feature, label = get_input_data('data/8000hz',
                                         5,
                                         200)
+        print(feature.shape)
+
         for i in range(100):
             print(sess.run([feature, label]))
